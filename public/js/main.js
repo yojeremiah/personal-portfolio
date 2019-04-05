25761c31e2b24d2947b15ee50b60baacf7143641
Jeremiah S <jschultzdev@gmail.com>
2019-04-04 21:24:11 -0700

    Add email form modal styles and js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,32 @@
-console.log('Loaded');
+// Wait for page to load scripts
+window.onload = function() {
+  console.log('Portfolio has loaded');
+
+  // * Email Modal + Overlay
+  // Grab the modal overlay which contains the email form
+  let modalOverlay = document.getElementById('email-modal');
+
+  // Grab the button that opens the modal when clicked
+  let emailBtn = document.getElementById('btn-email');
+
+  // Grab the <span> element (x) that closes the modal when clicked
+  let closeModalX = document.getElementsByClassName('close-modal-x')[0];
+
+  // When the user clicks on the submit btn, open the modal
+  emailBtn.onclick = function() {
+    modalOverlay.style.display = 'flex';
+    console.log('clicked button');
+  };
+
+  // When the user clicks on the <span> (x), close the modal
+  closeModalX.onclick = function() {
+    modalOverlay.style.display = 'none';
+  };
+
+  // When the user clicks anywhere outside of the modal form, close the modal
+  window.onclick = function(event) {
+    if (event.target == modalOverlay) {
+      modalOverlay.style.display = 'none';
+    }
+  };
+};
